f29c358407f2945f5d39962652cbc11b342dc935
Theoldwei <1814687573@qq.com>
2022-05-19 13:33:52 +0800

    修改代理地址

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,8 @@ module.exports = {
     proxy: {
       "/api": {
         // 只对请求路由以/api开头的请求进行代理转发
-        target: "http://39.98.123.211", // 转发的目标url
+        // 转发的目标url
+        target: "http://gmall-h5-api.atguigu.cn",
         changeOrigin: true, // 支持跨域
       },
     },
